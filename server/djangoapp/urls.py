5a91c6d2f0520aa70079bc823ca658edcdbdc80c
MuhammadBurhan123 <burhantheschoolboy@gmail.com>
2024-10-30 16:07:51 +0500

    Changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,16 +1,30 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+
+    # path for about view
+
+    # path for contact us view
+
+    # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
 
+    # path for logout
+
+    path(route='', view=views.get_dealerships, name='index'),
+    path('contact/', view=views.contact_page_view, name='contact'),
+    path('about/', view=views.about_page_view, name='about'),
+    path('logout/', views.logout_request, name='logout'),
+    path('login/', views.login_request, name='login'),
+    path('registration/', views.registration_request, name='registration'),
     # path for dealer reviews view
 
     # path for add a review view
